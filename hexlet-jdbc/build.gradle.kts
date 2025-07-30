14a6c9045ce0e568aaef84ce9ffbbc472dc3b96c
wasiliyterkin46 <wasiliy_terkin@mail.ru>
2025-07-30 13:39:46 +0300

    Init git

---
+++ hexlet-jdbc/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    application
+    checkstyle
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.h2database:h2:2.2.220")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+checkstyle {
+    toolVersion = "10.21.4"
+}
